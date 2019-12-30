0a92181671553b49297ccd250c30900d848ca35a
ashrakrahman <ashrakrahman@gmail.com>
2019-12-30 23:46:24 +0600

    test 2

---
+++ build/precache-manifest.13b461ace1bfa4e85a11fc06d592922f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10eb83694064c696e5255677a5be7aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97b64ee00f6f4f13bd53",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d6cf3e0165d22fc1079f",
+    "url": "/static/js/2.53920c02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.53920c02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97b64ee00f6f4f13bd53",
+    "url": "/static/js/main.3418bd6b.chunk.js"
+  },
+  {
+    "revision": "07e84654bf1a348d4a0a",
+    "url": "/static/js/runtime-main.21d511e1.js"
+  }
+]);
